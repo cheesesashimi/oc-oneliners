ffd7ef64a5ae60f4f141e901b7fe2877bcf21567
Zack Zlotnik <zzlotnik@redhat.com>
2022-02-23 11:32:47 -0500

    analyze_owners: improved alias handling

--- analyze_owners.py
+++ analyze_owners.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
+
 import os
 import sys
 import yaml
 
-def read_owners_file(filename):
-    with open(filename, "r") as owners_file:
-        return list(yaml.safe_load_all(owners_file))
+def load_file(filename):
+    with open(filename, "r") as loaded_file:
+        return list(yaml.safe_load_all(loaded_file))[0]
 
+# Find all the OWNERS files in a given repository, excluding the .git and
+# vendor directories for speed
 def find_owner_files(root_path):
     exclude = set(("vendor", ".git"))
 
@@ -17,27 +21,43 @@ def find_owner_files(root_path):
             if name == "OWNERS":
                 yield os.path.join(root, name)
 
-approvers = set()
-reviewers = set()
-components = set()
+def load_all_files(root_path):
+    return {
+        "owners_files": {filename: load_file(filename)
+                         for filename in find_owner_files(root_path)
+                         if filename != os.path.join(root_path, "OWNERS")},
+        "top_level_owners_file": load_file(os.path.join(root_path, "OWNERS")),
+        "owners_aliases": {alias: set(team)
+                           for alias, team in load_file(os.path.join(root_path, "OWNERS_ALIASES"))["aliases"].items()}
+    }
 
-for owners_file in find_owner_files("."):
-    for doc in read_owners_file(owners_file):
-        if doc.get("approvers"):
-            for approver in doc["approvers"]:
-                approvers.add(approver)
+def lookup_known_aliases(approvers, reviewers, loaded_files):
+    owners_aliases = loaded_files["owners_aliases"]
+    top_level_aliases = set(loaded_files["top_level_owners_file"]["approvers"]).union(loaded_files["top_level_owners_file"]["reviewers"])
+    found_aliases = set(approvers.keys()).union(set(reviewers.keys())).union(top_level_aliases)
+    return {alias: sorted(list(owners_aliases[alias]))
+            for alias in found_aliases
+            if alias in owners_aliases}
 
-        if doc.get("reviewers"):
-            for reviewer in doc["reviewers"]:
-                reviewers.add(reviewer)
+approvers_by_file = defaultdict(list)
+reviewers_by_file = defaultdict(list)
 
-        if doc.get("component"):
-            components.add(doc["component"])
+loaded_files = load_all_files(sys.argv[1])
+
+for owners_file, doc in loaded_files["owners_files"].items():
+    if doc.get("approvers"):
+        for approver in doc["approvers"]:
+            approvers_by_file[approver].append(owners_file)
+
+    if doc.get("reviewers"):
+        for reviewer in doc["reviewers"]:
+            reviewers_by_file[reviewer].append(owners_file)
 
 out = {
-    "approvers": sorted(list(approvers)),
-    "reviewers": sorted(list(reviewers)),
-    "components": sorted(list(components)),
+    "approvers_by_file": dict(approvers_by_file),
+    "reviewers_by_file": dict(reviewers_by_file),
+    "reviewers_by_alias": lookup_known_aliases(approvers_by_file, reviewers_by_file, loaded_files),
+    "top_level_owners": loaded_files["top_level_owners_file"],
 }
 
 print(yaml.safe_dump(out))
