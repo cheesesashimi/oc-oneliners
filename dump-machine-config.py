19f2254608be656671d04b9b5df1d017dfb7503e
Zack Zlotnik <zzlotnik@redhat.com>
2022-02-24 10:43:33 -0500

    dump-machine-config: error handling

--- dump-machine-config.py
+++ dump-machine-config.py
@@ -24,18 +24,29 @@ def load_machineconfig(filename):
 
 def get_contents(raw_content):
   unquoted = unquote(raw_content.replace("data:,", ""))
+  # If empty, stop here.
   if unquoted == "":
     return unquoted
 
   try:
+    # If we have JSON, lets pretty-print it!
     return json.dumps(json.loads(unquoted), indent=4, sort_keys=True)
   except json.decoder.JSONDecodeError:
+    # We didn't have any JSON, so lets return
     return unquoted
 
 out = {}
 
 for doc in load_machineconfig(sys.argv[1]):
-    for mc_file in doc["spec"]["config"]["storage"]["files"]:
-        out[mc_file["path"]] = get_contents(mc_file["contents"]["source"])
+  for mc_file in doc["spec"]["config"]["storage"]["files"]:
+    contents = mc_file.get("contents")
+    if not contents:
+      continue
+
+    source = contents.get("source")
+    if not source:
+      continue
+
+    out[mc_file["path"]] = get_contents(source)
 
 print(yaml.safe_dump(out))
