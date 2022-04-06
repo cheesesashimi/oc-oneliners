3ea13bfed8182ab184bc887b80c10336c7925e24
Zack Zlotnik <zzlotnik@redhat.com>
2022-04-06 17:09:06 -0400

    adds script to delete a machine given a node name

---
+++ delete-machine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+node_name="$1"
+
+machine_id="$(oc get "node/$node_name" -o json | jq -r '.metadata.annotations["machine.openshift.io/machine"]' | sed 's/openshift-machine-api\///g')"
+
+oc delete --wait=false "machine/$machine_id" -n openshift-machine-api
+oc delete --wait=false "node/$node_name"
