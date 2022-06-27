0de468cc460216a8b278f604f22f00923589bd3b
Zack Zlotnik <zzlotnik@redhat.com>
2022-06-27 11:12:14 -0400

    delete-machine: use jsonpath to eliminate jq

--- delete-machine.sh
+++ delete-machine.sh
@@ -4,7 +4,7 @@ set -x
 
 node_name="$1"
 
-machine_id="$(oc get "node/$node_name" -o json | jq -r '.metadata.annotations["machine.openshift.io/machine"]' | sed 's/openshift-machine-api\///g')"
+machine_id="$(oc get "node/$node_name" -o jsonpath='{.metadata.annotations.machine\.openshift\.io/machine}' | sed 's/openshift-machine-api\///g')"
 
 oc delete --wait=false "machine/$machine_id" -n openshift-machine-api
 oc delete --wait=false "node/$node_name"
