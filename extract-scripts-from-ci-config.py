404c9ce7ce42d4fab0dbd7f7096efd4507625ee4
Zack Zlotnik <zzlotnik@redhat.com>
2022-06-28 09:48:09 -0400

    adds support for top-level test commands

--- extract-scripts-from-ci-config.py
+++ extract-scripts-from-ci-config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 # This script extracts all the scripts from the tests section in a given
-# OpenShift CI config file. Useful for extracting scripts for static analysis.
-# Currently only supports tests which define multiple steps.
+# OpenShift CI config file. Useful for extracting scripts for static analysis
+# or relocation into one's project repo.
 
 # Usage:
 # $ ./extract-scripts-from-ci-config.py './ci-operator/config/path/to/config'
@@ -19,10 +19,21 @@ config_path = sys.argv[1]
 config = load_config(config_path)
 print("Loaded config from", config_path)
 for test in config["tests"]:
-    if not test.get("steps"):
+    if test.get("commands"):
+        outfilename = "{test_name}.sh".format(test_name=test["as"])
+        print("Writing script for {test_name} to {outfilename}".format(test_name=test["as"], outfilename=outfilename))
+        with open(outfilename, "w") as outfile:
+            outfile.write(test["commands"])
+
+    steps = test.get("steps")
+    if not steps:
         continue
 
-    outfilename = test["as"] + "-test.sh"
-    print("Writing script for {test_name} to {outfilename}".format(test_name=test["as"], outfilename=outfilename))
-    with open(outfilename, "w") as outfile:
-        outfile.write(test["steps"]["test"][0]["commands"])
+    if not steps.get("test"):
+        continue
+
+    for step in test["steps"]["test"]:
+        outfilename = "{test_name}-{step_name}.sh".format(test_name=test["as"], step_name=step["as"])
+        print("Writing script for {test_name}-{step_name} to {outfilename}".format(test_name=test["as"], step_name=step["as"], outfilename=outfilename))
+        with open(outfilename, "w") as outfile:
+            outfile.write(step["commands"])
