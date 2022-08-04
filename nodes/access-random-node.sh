07c4747627c640c8615ef9ddd7cd4bf9eba8099f
Zack Zlotnik <zzlotnik@redhat.com>
2022-08-04 15:54:40 -0400

    nodes: adds helpers for working with nodes

--- nodes/access-random-node.sh
+++ nodes/access-random-node.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-set -xeuo
-
 role="$1"
-node="$(oc get nodes --selector "node-role.kubernetes.io/$role=" -o json | jq -r '.items[0].metadata.name')"
+nodes="$(oc get nodes --selector "node-role.kubernetes.io/$role=" -o json | jq -r -c)"
+num_nodes="$(echo "$nodes" | jq -r '.items | length')"
+index=$(( $RANDOM % $num_nodes ))
+node="$(echo "$nodes" | jq --argjson index "$index" -r '.items[$index].metadata.name')"
+echo "Targetting node/$node..."
 oc debug "node/$node"
