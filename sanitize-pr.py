5f0b103a1646868e85e4f7dfc692aaf73223c15a
Zack Zlotnik <zzlotnik@redhat.com>
2023-03-30 12:18:12 -0400

    rewrite sanitize PR script in python

---
+++ sanitize-pr.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import subprocess
+import json
+
+def delete_comment(comment):
+    url = comment["url"].replace("https://api.github.com/", "")
+    subprocess.run(["gh", "api", "--method", "DELETE", "-H", "Accept: application/vnd.github.v3+json", url])
+    print("Deleted:", comment["body"])
+
+def find_and_delete_comments(opts, username):
+    comments_path = "repos/%(org)s/%(repo)s/issues/%(pr)s/comments" % opts
+    comments_output = subprocess.run(["gh", "api", "-H", "Accept: application/vnd.github.v3+json", comments_path], capture_output=True)
+    parsed = json.loads(comments_output.stdout)
+
+    for comment in parsed:
+        comment_body = comment["body"]
+        commentor_username = comment["user"]["login"]
+
+        if commentor_username == username and comment_body.startswith("/test"):
+            delete_comment(comment)
+
+if __name__ == "__main__":
+    username = "cheesesashimi"
+
+    opts = {
+        "org": "openshift",
+        "repo": "machine-config-operator",
+        "pr": "3649",
+    }
+
+    prs = ["3649", "3637"]
+
+    for pr in prs:
+        opts["pr"] = pr
+        find_and_delete_comments(opts, username)
